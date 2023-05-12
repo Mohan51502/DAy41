5234e6515f49550f0efb6e3eccfff2d6a49b8811
Mohan51502 <rajmohan51502@gmail.com>
2023-05-12 16:00:04 +0530

    second commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyparser = require("body-parser");
 const APP_SERVER = require("./app");
 const NODE_SERVER = express();
+require('dotenv').config()
 
 /**
  * ADDING MIDDLEWARES TO NODE_SERVER
@@ -23,7 +24,7 @@ NODE_SERVER.use("/api", APP_SERVER);
  * CONFIGURE NODE SERVER
  * PORT TO ACCEPT/SEND REQUEST/RESPONSE = 5000
  */
-const PORT = 5000;
-NODE_SERVER.listen(PORT, "localhost", () => {
+const port = process.env.PORT || 5000;
+NODE_SERVER.listen(port, "localhost", () => {
   console.log("SERVER STARTED ON PORT", PORT);
 });
