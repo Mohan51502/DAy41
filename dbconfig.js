47b8f14930942616cd6d8247a4a58fb4c96c0c8b
Mohan51502 <rajmohan51502@gmail.com>
2023-05-12 15:46:09 +0530

    first commit

---
+++ dbconfig.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+mongoose.connect("mongodb+srv://rajmohan51502:vo5zORfxndu72DSl@cluster0.wwgxqxn.mongodb.net/products");
+
+
+// async function db(){
+
+// }
+// db()
+// module.exports
